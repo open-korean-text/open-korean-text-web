bbefd9f3d81179c5ad5dc0581114eee213285b20
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-04-05 23:08:46 -0700

    Added eval

--- public/js/eval_control.jsx
+++ public/js/eval_control.jsx
@@ -1,45 +1,183 @@
 class VoteGroup extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {selectedAction: null};
+    this.state = {selectedCandidate: null};
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick(selectedAction) {
-    this.setState({selectedAction: selectedAction})
-    requestProcess(selectedAction, koreanTextInput())
+  handleClick(selectedCandidate) {
+    this.setState({selectedCandidate: selectedCandidate})
+    $("#submit_button").removeClass("disabled");
+  }
+
+  handleSubmit() {
+    var ref = defaultDatabase.ref('/tokenization_votes/' + userUID + "/" + questionNumber + "/");
+
+    ref.set(this.state.selectedCandidate);
+
+    location.reload();
   }
 
   render() {
-    return <div className="collection">
-      <a href="#!" className="collection-item">Alvin</a>
-      <a href="#!" className="collection-item active">Alvin</a>
-      <a href="#!" className="collection-item">Alvin</a>
-      <a href="#!" className="collection-item">Alvin</a>
-    </div>
+    var data = this.props.data
+
+    var keys = Object.keys(this.props.data).filter(function (key) {
+      return key.startsWith("tokenization") && data[key]
+    });
+
+    var thisPointer = this;
+
+    var answers = keys.map(function (key) {
+      return <Answer key={key}
+                     answerKey={key}
+                     answer={data[key]}
+                     clickAction={thisPointer.handleClick.bind(thisPointer, key)}
+                     current={thisPointer.state.selectedCandidate}
+      />
+    })
+
+    defaultDatabase.ref('/tokenization_votes/' + userUID).once('value').then(function (snapshot) {
+      var prevAnswers = snapshot.val();
+      var prevAnswerSize = Object.keys(prevAnswers).length;
+
+      if (prevAnswers[questionNumber] && prevAnswerSize < 3000) {
+        location.reload();
+      }
+
+      ReactDOM.render(
+        <div>{prevAnswerSize} / 3000</div>,
+        document.getElementById('prev_answer_count')
+      );
+    });
+
+    return (
+      <div className="problem">
+        <div className="right" id="prev_answer_count"></div>
+
+        <div className="question">
+          <blockquote>{data["chunk"]}
+            <a href={"https://www.google.com/search?q=" + data["chunk"]} target="_blank">
+              <img src="img/search.png" className="search_icon"/>
+            </a>
+          </blockquote>
+        </div>
+        <div className="collection">
+          {answers}
+          <Answer key="wrong"
+                  answerKey="wrong"
+                  answer="모두 틀렸음"
+                  clickAction={this.handleClick.bind(this, "wrong")}
+                  current={this.state.selectedCandidate}
+          />
+        </div>
+        <a className="submit_button btn disabled waves-effect waves-light right"
+           id="submit_button"
+           onClick={this.handleSubmit}
+        >보내기</a>
+      </div>
+    )
   }
 }
 
-class ActionButton extends React.Component {
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
+class Answer extends React.Component {
   constructor(props) {
     super(props);
   }
 
   isActive() {
-    return this.props.current === this.props.request ? "active_button" : ""
+    return this.props.current === this.props.answerKey ? "active" : ""
   }
 
   render() {
+    var parse = this.props.answer
+    parse = replaceAll(parse, "Adverb", "부사");
+    parse = replaceAll(parse, "Verb", "동사");
+    parse = replaceAll(parse, ": [0-9]+, [0-9]+", "");
+    parse = replaceAll(parse, "PreEomi", "선어말어미");
+    parse = replaceAll(parse, "Eomi", "어미");
+    parse = replaceAll(parse, "Adjective", "형용사");
+    parse = replaceAll(parse, "Noun", "명사");
+    parse = replaceAll(parse, "Josa", "조사");
+    parse = replaceAll(parse, "Suffix", "접미사");
+    parse = replaceAll(parse, "Prefix", "접두사");
+    parse = replaceAll(parse, "Determiner", "지시사");
+    parse = replaceAll(parse, "Exclamation", "감탄사");
+    parse = replaceAll(parse, "/", " / ");
+
     return (
-      <li className=
-            {'action_button black-text grey lighten-4 waves-effect waves-dark ' + this.isActive()}
-          onClick={this.props.clickAction}>
-        {this.props.action}
-      </li>
+      <a href="#!" className={"collection-item waves-effect waves-light " + this.isActive()}
+         onClick={this.props.clickAction}
+      >{parse}</a>
     );
   }
 }
 
-ReactDOM.render(
-  <VoteGroup/>,
-  document.getElementById('vote_container')
-);
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: 'eval.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//        firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+};
+
+var userUID;
+var userData;
+
+window.addEventListener('load', function () {
+  var logout = function () {
+    firebase.auth().signOut().then(function () {
+      $("#logged_in_container").hide();
+      location.reload();
+    }).catch(function (error) {
+      // An error happened.
+      alert(error);
+    });
+  }
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      $("#logged_in_container").show();
+      $("#logged_out_container").hide();
+
+      userUID = user.uid;
+      userData = user.providerData[0];
+      // document.getElementById('sign-in-status').textContent = "Hi, " + user.providerData[0]["displayName"] + "!";
+      ReactDOM.render(
+        <div className="chip">
+          <img src={userData["photoURL"]}/>
+          {userData["displayName"]}
+          <a href="#!" onClick={logout}> [sign out]</a>
+        </div>,
+        document.getElementById('user-info')
+      );
+
+      ref.once('value').then(function (snapshot) {
+        ReactDOM.render(
+          <VoteGroup data={snapshot.val()}/>,
+          document.getElementById('vote_container')
+        );
+      });
+    } else {
+      $("#logged_out_container").show();
+      $("#logged_in_container").hide();
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
+});
+
+
+
+
+
--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,8 @@
 // Initialize Firebase
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 var config = {
   apiKey: "AIzaSyAqru-W24WUNZj8SuKnOnlUo3Pcuqme5jA",
   authDomain: "open-korean-text-web.firebaseapp.com",
@@ -7,33 +11,11 @@ var config = {
   storageBucket: "open-korean-text-web.appspot.com",
   messagingSenderId: "1026312463332"
 };
+
 var defaultApp = firebase.initializeApp(config);
 
 var defaultDatabase = defaultApp.database();
 
-var ref = defaultDatabase.ref('/tokenization_examples/' + getRandomInt(1, 42301));
+var questionNumber = getRandomInt(1, 3000)
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// var provider = new firebase.auth.GoogleAuthProvider();
-//
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-
-console.log(getRandomInt(1, 42301))
+var ref = defaultDatabase.ref('/tokenization_examples/' + questionNumber);
